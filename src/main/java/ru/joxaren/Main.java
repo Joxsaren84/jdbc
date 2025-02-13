6d0be5368c953462bc1880c69ee6c33812ee2a78
Keypa <42-18@mail.ru>
2025-02-13 14:07:46 +1000

    Add class ConnectionManager

--- src/main/java/ru/joxaren/Main.java
+++ src/main/java/ru/joxaren/Main.java
@@ -1,10 +1,20 @@
 package ru.joxaren;
 
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args){
 
-
-
+   // Class <Driver> driverClass = Driver.class;
+        try (Connection connection = ConnectionManager.open()){
+            System.out.println(connection.getTransactionIsolation());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
