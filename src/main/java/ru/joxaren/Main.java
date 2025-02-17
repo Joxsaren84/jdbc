ad419e9c248dcbee760e384dc3bb804a52a3f779
Keypa <42-18@mail.ru>
2025-02-17 12:13:34 +1000

    using statement

--- src/main/java/ru/joxaren/Main.java
+++ src/main/java/ru/joxaren/Main.java
@@ -5,13 +5,22 @@ import org.postgresql.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args){
 
+        String sql = "CREATE TABLE IF NOT EXISTS info" +
+                "(id SERIAL PRIMARY KEY," +
+                "data TEXT NOT NULL);";
+
    // Class <Driver> driverClass = Driver.class;
-        try (Connection connection = ConnectionManager.open()){
-            System.out.println(connection.getTransactionIsolation());
+        try (Connection connection = ConnectionManager.open();
+            Statement statement = connection.createStatement()){
+
+            boolean executeResult = statement.execute(sql);
+            System.out.println(executeResult);
+            //System.out.println(connection.getTransactionIsolation());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
