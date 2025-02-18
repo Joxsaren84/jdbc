db3443d59c21211dbcd39ad4c7d3734ac5205d23
Keypa <42-18@mail.ru>
2025-02-18 15:08:04 +1000

    using statement execute & executeUpdate

--- src/main/java/ru/joxaren/Main.java
+++ src/main/java/ru/joxaren/Main.java
@@ -10,16 +10,30 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args){
 
+        /*
         String sql = "CREATE TABLE IF NOT EXISTS info" +
                 "(id SERIAL PRIMARY KEY," +
                 "data TEXT NOT NULL);";
+         */
+
+        String sql = "insert into info (data) values" +
+                " ('test1')," +
+                " ('test2')," +
+                " ('test3')," +
+                " ('test4');"+
+        " insert into info (data) values" +
+                " ('test1')," +
+                " ('test2')," +
+                " ('test3')," +
+                " ('test4');";
 
    // Class <Driver> driverClass = Driver.class;
         try (Connection connection = ConnectionManager.open();
             Statement statement = connection.createStatement()){
 
-            boolean executeResult = statement.execute(sql);
-            System.out.println(executeResult);
+            int executeUpdate = statement.executeUpdate(sql);
+            System.out.println(executeUpdate);
+            System.out.println(statement.getUpdateCount());
             //System.out.println(connection.getTransactionIsolation());
         } catch (SQLException e) {
             throw new RuntimeException(e);
