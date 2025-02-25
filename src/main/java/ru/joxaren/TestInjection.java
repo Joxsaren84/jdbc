80a3c00af2775f5df7cb158fa29865f9547c77bf
Keypa <42-18@mail.ru>
2025-02-25 12:21:21 +1000

    example with connection.PrepareStatement (preparedStatement.setFetchSize, preparedStatement.setQueryTimeout, preparedStatement.setMaxRows)

--- src/main/java/ru/joxaren/TestInjection.java
+++ src/main/java/ru/joxaren/TestInjection.java
@@ -1,28 +1,57 @@
 package ru.joxaren;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestInjection {
     public static void main(String[] args) {
-        String flightId = "2 or 1 = 1" ;
+        Long flightId = 2L;
         System.out.println(getTicketsByFlightId(flightId));
+        LocalDateTime start = LocalDateTime.of(2020, 3, 10, 0, 0);
+        LocalDateTime end = LocalDateTime.now();
+        System.out.println(flightBetween(start, end));
 
     }
 
-    private static List<Long> getTicketsByFlightId(String flightId){
-        String sql = "select id from ticket where flight_id = " + flightId;
+
+    private static List<Long> flightBetween(LocalDateTime start, LocalDateTime end){
+        String sql = "select id from flight where departure_date between ? and ?";
 
         List<Long> result = new ArrayList<>();
 
         try(Connection connection = ConnectionManager.open()){
-            Statement statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
 
-            ResultSet resultSet = statement.executeQuery(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                result.add(resultSet.getObject("id", Long.class));
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
+        return result;
+
+    }
+
+    private static List<Long> getTicketsByFlightId(Long flightId){
+        String sql = "select id from ticket where flight_id = ?";
+
+        List<Long> result = new ArrayList<>();
+
+
+        try(Connection connection = ConnectionManager.open()){
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setFetchSize(50);
+            preparedStatement.setQueryTimeout(10);
+            preparedStatement.setMaxRows(100);
+            preparedStatement.setLong(1, flightId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
                 result.add(resultSet.getObject("id", Long.class));
             }
