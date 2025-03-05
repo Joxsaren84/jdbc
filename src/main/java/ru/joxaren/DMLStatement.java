8bec6f0303569038c91530cbb37d35942c430ff0
Keypa <42-18@mail.ru>
2025-03-05 17:03:58 +1000

    example DMLStatement in single class

---
+++ src/main/java/ru/joxaren/DMLStatement.java
@@ -0,0 +1,26 @@
+package ru.joxaren;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DMLStatement {
+
+    public static void main(String[] args) {
+        String sql = "insert into info (data) values" +
+                " ('test1')," +
+                " ('test2')," +
+                " ('test3')," +
+                " ('test4');";
+
+        try(Connection connection = ConnectionManager.open()){
+            Statement statement = connection.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println(result);
+            System.out.println(statement.getUpdateCount());
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
