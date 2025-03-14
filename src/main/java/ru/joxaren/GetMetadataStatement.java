95ef087dfda71710a10400fa18461b2a0d4a6f88
Keypa <42-18@mail.ru>
2025-03-14 12:14:51 +1000

    example GetMetadataStatement in single class

---
+++ src/main/java/ru/joxaren/GetMetadataStatement.java
@@ -0,0 +1,38 @@
+package ru.joxaren;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetMetadataStatement {
+
+    public static void main(String[] args) {
+        checkMetaData();
+    }
+
+    private static void checkMetaData() {
+        try (Connection connection = ConnectionManager.open()) {
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+            ResultSet catalogs = databaseMetaData.getCatalogs();
+
+            while (catalogs.next()) {
+                System.out.println("Catalog:");
+                System.out.println(catalogs.getString(1));
+                System.out.println("  schemas:");
+                ResultSet schemas = databaseMetaData.getSchemas();
+                while (schemas.next()) {
+                    System.out.println("   " + schemas.getString(1));
+
+                    ResultSet getTables = databaseMetaData.getTables(null, null, "%", null);
+                    while (getTables.next()) {
+                        System.out.println(getTables.getString("TABLE_NAME"));
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
