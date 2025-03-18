4e8f85a3da4333c4ca4490ae51b8eee6325f2d93
Keypa <42-18@mail.ru>
2025-03-18 12:28:13 +1000

    example BatchStatement in single class

---
+++ src/main/java/ru/joxaren/BatchStatement.java
@@ -0,0 +1,47 @@
+package ru.joxaren;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BatchStatement {
+
+    public static void main(String[] args) throws SQLException {
+
+        final long flightId = 9L;
+
+        String sql1 = "DELETE FROM flight WHERE id = " + flightId;
+        String sql2 = "DELETE FROM ticket WHERE flight_id = " + flightId;
+
+
+        Connection connection = null;
+        Statement statement = null;
+
+
+        try{
+            connection = ConnectionManager.open();
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+
+            statement.addBatch(sql2);
+            statement.addBatch(sql1);
+
+            int[] result = statement.executeBatch();
+
+            connection.commit();
+
+        }catch (Exception e){
+            if(connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        }finally {
+            if(connection != null)
+                connection.close();
+            if (statement != null)
+                statement.close();
+        }
+    }
+
+
+}
